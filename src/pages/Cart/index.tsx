eff058f2b08155c4f505b89b1565f0aa6c3435af
Bruno Toresan <bruno.toresan@axur.com>
2021-07-01 19:26:06 -0300

    fixed CSS error

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { useEffect } from 'react';
 import {
   MdDelete,
   MdAddCircleOutline,
@@ -58,69 +56,69 @@ const Cart = (): JSX.Element => {
 
   return (
     <Container>
-      {cartFormatted.map(product => {
-        return(
-          <ProductTable key={product.id}>
-            <thead>
-              <tr>
-                <th aria-label="product image" />
-                <th>PRODUTO</th>
-                <th>QTD</th>
-                <th>SUBTOTAL</th>
-                <th aria-label="delete icon" />
-              </tr>
-            </thead>
-            <tbody>
-              <tr data-testid="product">
-                <td>
-                  <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
-                </td>
-                <td>
-                  <strong>{product.title}</strong>
-                  <span>{product.priceFormatted}</span>
-                </td>
-                <td>
-                  <div>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th aria-label="product image" />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th aria-label="delete icon" />
+          </tr>
+        </thead>
+        <tbody>
+            {cartFormatted.map(product => {
+              return (
+                <tr data-testid="product" key={product.id}>
+                  <td>
+                    <img src={product.image} alt={product.title} />
+                  </td>
+                  <td>
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
+                  </td>
+                  <td>
+                    <div>
+                      <button
+                        type="button"
+                        data-testid="decrement-product"
+                        disabled={product.amount <= 1}
+                        onClick={() => handleProductDecrement(product)}
+                      >
+                        <MdRemoveCircleOutline size={20} />
+                      </button>
+                      <input
+                        type="text"
+                        data-testid="product-amount"
+                        readOnly
+                        value={product.amount}
+                      />
+                      <button
+                        type="button"
+                        data-testid="increment-product"
+                        onClick={() => handleProductIncrement(product)}
+                      >
+                        <MdAddCircleOutline size={20} />
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <strong>{product.subTotal}</strong>
+                  </td>
+                  <td>
                     <button
                       type="button"
-                      data-testid="decrement-product"
-                      disabled={product.amount <= 1}
-                      onClick={() => handleProductDecrement(product)}
+                      data-testid="remove-product"
+                      onClick={() => handleRemoveProduct(product.id)}
                     >
-                      <MdRemoveCircleOutline size={20} />
+                      <MdDelete size={20} />
                     </button>
-                    <input
-                      type="text"
-                      data-testid="product-amount"
-                      readOnly
-                      value={product.amount}
-                    />
-                    <button
-                      type="button"
-                      data-testid="increment-product"
-                      onClick={() => handleProductIncrement(product)}
-                    >
-                      <MdAddCircleOutline size={20} />
-                    </button>
-                  </div>
-                </td>
-                <td>
-                  <strong>{product.subTotal}</strong>
-                </td>
-                <td>
-                  <button
-                    type="button"
-                    data-testid="remove-product"
-                    onClick={() => handleRemoveProduct(product.id)}
-                  >
-                    <MdDelete size={20} />
-                  </button>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+            )
+            })}
             </tbody>
           </ProductTable>
-        )
-      })}
 
       <footer>
         <button type="button">Finalizar pedido</button>
